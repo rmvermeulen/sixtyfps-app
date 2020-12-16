6cbc652a2264281fca29aca69ee92dc357443e9d
Rasmus Vermeulen <rasmus_vermeulen@tutanota.com>
2020-12-16 19:04:47 +0100

    Sample app

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,33 @@
 sixtyfps::sixtyfps! {
-    HelloWorld := Rectangle {
-        Text {
-            text:"Hi there (from macro)";
-            color:blue;
+    import {
+        Button, CheckBox, SpinBox, Slider,
+        GroupBox, LineEdit, StandardListView, ComboBox
+    } from "sixtyfps_widgets.60";
+
+    HelloWorld := Window {
+        signal doThing;
+        property<string> header_text <=> input.text;
+        GridLayout {
+
+            Rectangle {
+                Text {
+                    text: header_text;
+                    color: blue;
+                    width: 50%;
+                }
+                color: red;
+                height: 20%;
+            }
+            width: 800px;
+            height: 500px;
+            input := TextInput {
+                text: "thisisaninput";
+            }
+            Button {
+                text: "Regular Button";
+                enabled: true;
+                clicked => { root.doThing() }
+            }
         }
     }
 }
@@ -10,5 +35,9 @@ sixtyfps::sixtyfps! {
 // sixtyfps::include_modules!();
 
 fn main() {
-    HelloWorld::new().run();
+    let app = HelloWorld::new();
+    app.on_doThing(|| {
+        println!("button clicked!");
+    });
+    app.run();
 }
