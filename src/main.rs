4de1541c1e9ee8986cbf97a20ef433714641908e
Rasmus Vermeulen <rasmus_vermeulen@tutanota.com>
2020-12-16 20:00:43 +0100

    Trying to set an array property

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,7 @@ sixtyfps::sixtyfps! {
     MyApp := Window {
         signal doThing;
         property<string> header_text <=> input.text;
-        property<[string]> files: [];
+        property<[StandardListViewItem]>files <=> list.model;
         GridLayout {
 
             Rectangle {
@@ -29,28 +29,53 @@ sixtyfps::sixtyfps! {
                 enabled: true;
                 clicked => { root.doThing() }
             }
-        }
-        StandardListView {
+            list := StandardListView {
+                model: [
+                    {text:"file1"},
+                    {text:"file2"},
+                    {text:"file3"}
+                ];
+            }
         }
     }
 }
-
+struct StandardListViewItem {
+    text: sixtyfps::SharedString,
+}
 // sixtyfps::include_modules!();
 
 fn main() {
-    do_a_glob();
     run_app();
+    // do_a_glob();
 }
+
 fn run_app() {
+    use sixtyfps::SharedString;
     let app = MyApp::new();
     app.on_doThing(|| {
         println!("button clicked!");
+        use glob::glob;
+        let files = glob("./src/*.rs")
+            .unwrap()
+            .filter_map(|entry| match entry {
+                Ok(path) => Some(path),
+                Err(e) => None,
+            })
+            .filter_map(|path| path.to_str())
+            .map(|str| {
+                Box::new(StandardListViewItem {
+                    text: SharedString::from(str),
+                })
+            })
+            .collect();
+        app.set_files(sixtyfps::ModelHandle::new(files));
     });
     app.run();
 }
+
 fn do_a_glob() {
     use glob::glob;
-    for entry in glob("D:/assets/*.png").expect("Failed to read glob pattern") {
+    for entry in glob("./src/*.rs").expect("Failed to read glob pattern") {
         match entry {
             Ok(path) => {
                 println!("{:?}", path.display());
